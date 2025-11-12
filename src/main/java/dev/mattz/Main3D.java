8f78a0026d14994fae28eb4cd041d0af450fa692
Matouš Marciš <74117837+MaTTzCZ@users.noreply.github.com>
2025-11-12 19:06:02 +0100

    minor custom classes update

--- src/main/java/dev/mattz/Main3D.java
+++ src/main/java/dev/mattz/Main3D.java
@@ -1,11 +1,17 @@
 package dev.mattz;
 
+import dev.mattz.data.D3.vector;
 import dev.mattz.data.D3.matrix.Matrix;
+import dev.mattz.data.D3.matrix.MatrixIdentity;
 
 public class Main3D {
     public static void main(String[] args) {
         Matrix matrix = new Matrix(5, 5);
-        System.out.println(matrix);
+        matrix.fillWithRandom(1, 10);
+        Matrix indentity = new MatrixIdentity(5);
+        vector vector = new vector(1, 2 , 3, 4, 5);
+        System.out.println(vector);
+        printMatrix(matrix);
     }
 
     private static void printMatrix(Matrix matrix) {
@@ -14,7 +20,7 @@ public class Main3D {
         for (int row = 0; row < rowCount; row++) {
             System.out.print("|");
             for (int column = 0; column < columnCount; column++) {
-                System.out.printf(" %2f ", matrix.getValue(row, column));
+                System.out.printf(" %.2g", matrix.getValue(row, column));
             }
             System.out.println("|");
         }
--- src/main/java/dev/mattz/data/D3/matrix/Matrix.java
+++ src/main/java/dev/mattz/data/D3/matrix/Matrix.java
@@ -1,12 +1,14 @@
 package dev.mattz.data.D3.matrix;
 
 import java.util.Arrays;
+import java.util.Optional;
+import java.util.Random;
 
 public class Matrix {
     private final double[][] matrix;
 
-    private int rows;
-    private int columns;
+    private final int rows;
+    private final int columns;
 
     public Matrix(int rows, int columns) {
         matrix = new double[rows][columns];
@@ -27,6 +29,21 @@ public class Matrix {
             }
         }
         return newMatrix;
+    }
+
+    public Optional<Matrix> multiply(Matrix matrix){
+        if (rows != matrix.columns)
+            return Optional.empty();
+    }
+
+    public void fillWithRandom(int min, int max) {
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                Random r = new Random();
+                int randomInt = r.nextInt(min, max);
+                matrix[row][column] = randomInt;
+            }
+        }
     }
 
     public double getValue(int row, int column) {
